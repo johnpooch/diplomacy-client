fce2e184ec8468485236220336ebc82586abf3f2
Sam Hayes <sam.hayes@gamer-network.net>
2020-04-16 15:49:30 +0100

    Use env for service URI

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-export const BASEURL = 'http://127.0.0.1:8082/api/v1/';
+const serviceURI = process.env.SERVICE_URI || 'http://127.0.0.1:8082/api/v1/';
 
 // Games
-export const ALLGAMESURL = `${BASEURL}games`;
-export const USERGAMESURL = `${BASEURL}games/mygames`;
-export const CREATEGAMEURL = `${BASEURL}games/create`;
+export const ALLGAMESURL = `${serviceURI}games`;
+export const USERGAMESURL = `${serviceURI}games/mygames`;
+export const CREATEGAMEURL = `${serviceURI}games/create`;
 
 // Game
-export const GAMESTATEURL = `${BASEURL}game/<int:game>`;
+export const GAMESTATEURL = `${serviceURI}game/<int:game>`;
