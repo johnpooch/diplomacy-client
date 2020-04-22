727804c5c652c199538c80dfd5d427020b091dbb
John McDowell <john.mcdowell@gamer-network.net>
2020-04-22 18:15:28 +0100

    Add component for browse game

--- src/views/BrowseGames.jsx
+++ src/views/BrowseGames.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import Alert from '../components/Alert';
+import BrowseGame from '../components/BrowseGame';
 import Heading from '../components/Heading';
 import Loading from '../components/Loading';
 import { PageWrapper } from '../globals';
@@ -15,52 +16,13 @@ const StyledList = styled.ol`
   list-style: none;
 `;
 
-const StyledListItem = styled.li`
-  &:not(:last-child) {
-    margin-bottom: ${spacing[5]}px;
-  }
-
-  a {
-    text-decoration: none;
-    color: ${colors.base};
-
-    &:hover .name {
-      text-decoration: underline;
-    }
-  }
-
-  header {
-    margin-bottom: ${spacing[2]}px;
-  }
-
-  p {
-    margin: 0;
-
-    &:not(:last-child) {
-      margin-bottom: ${spacing[2]}px;
-    }
-  }
-
-  .name {
-    font-weight: 600;
-  }
-
-  .id {
-    margin-left: ${spacing[1]}px;
-
-    &:before {
-      content: '#';
-    }
-  }
-
-  .label {
-    font-style: italic;
-
-    &:after {
-      content: ': ';
-    }
-  }
-`;
+function gameToData(game) {
+  return {
+    createdBy: game.created_by,
+    createdAt: game.created_at,
+    ...game,
+  };
+}
 
 class BrowseGames extends React.Component {
   constructor(props) {
@@ -101,51 +63,6 @@ class BrowseGames extends React.Component {
       });
   }
 
-  static getDateDisplayFormat() {
-    return {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-    };
-  }
-
-  static renderGamesListItem(game) {
-    const date = new Date(game.created_at);
-    const dateString = date.toLocaleDateString(
-      'en-GB',
-      BrowseGames.getDateDisplayFormat()
-    );
-
-    return (
-      <StyledListItem key={game.id}>
-        <Link to={`/game/${game.id}`}>
-          <header>
-            <span className="name">{game.name}</span>
-            <span className="id">{game.id}</span>
-          </header>
-          <main>
-            <p className="created-at">
-              <span className="label">Created</span>
-              <time className="value" dateTime={game.created_at}>
-                {dateString}
-              </time>
-            </p>
-            <p className="created-by">
-              <span className="label">By player</span>
-              <span className="value">{game.created_by}</span>
-            </p>
-            <p className="variant">
-              <span className="label">Variant</span>
-              <span className="value">{game.variant.name}</span>
-            </p>
-          </main>
-        </Link>
-      </StyledListItem>
-    );
-  }
-
   renderView() {
     const { isLoaded, games } = this.state;
 
@@ -159,7 +76,9 @@ class BrowseGames extends React.Component {
 
     const gamesList = [];
     games.forEach((game) => {
-      gamesList.push(BrowseGames.renderGamesListItem(game));
+      const data = gameToData(game);
+      console.log(data);
+      gamesList.push(<BrowseGame key={data.id} data={data} />);
     });
     return <StyledList>{gamesList}</StyledList>;
   }
