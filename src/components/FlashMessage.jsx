12c527d08fecffdf06e3e98ee5ae945a3876cbe1
John McDowell <john.mcdowell@gamer-network.net>
2020-05-05 17:45:56 +0100

    Add styling to close message button

--- src/components/FlashMessage.jsx
+++ src/components/FlashMessage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import { PageWrapper } from '../styles';
 import { colorMap, spacing, fontSizes } from '../variables';
 
 const StyledDiv = styled.div`
-  display: flex;
   width: 100%;
   height: max-content;
-  padding: ${spacing[3]}px;
   color: ${(props) => colorMap[props.type].text};
   background: ${(props) => colorMap[props.type].background};
   font-size: ${fontSizes.sans[2]}px;
@@ -17,12 +17,21 @@ const StyledDiv = styled.div`
     margin-bottom: ${spacing[4]}px;
   }
   .close {
-    color: black;
-    font-weight: bolder;
+    padding: 1px 5px;
+    color: ${(props) => colorMap[props.type].text};
     cursor: pointer;
-    border: solid 1px black;
-    margin-right: 10px;
-    border-radius: 4px;
+    border: none;
+    float: right;
+    margin: 0.5rem;
+    background: inherit;
+  }
+  .close:hover {
+    color: white;
+    background: ${(props) => colorMap[props.type].text};
+    border-radius: 100%;
+  }
+  p {
+    padding: 1rem;
   }
 `;
 
@@ -47,7 +56,7 @@ class FlashMessage extends React.Component {
         <PageWrapper>
           <StyledDiv type={type}>
             <button type="button" className="close" onClick={this.close}>
-              x
+              <FontAwesomeIcon icon={faTimes} />
             </button>
             <p>{text}</p>
           </StyledDiv>
