b07c9187d6f2d6c8470c946779229a18bba359e6
John McDowell <john.mcdowell@gamer-network.net>
2020-06-16 09:54:17 +0100

    Convert GameStatus component from class into function

--- src/components/GameSummary.jsx
+++ src/components/GameSummary.jsx
@@ -43,40 +43,47 @@ const StyledListItem = styled.li`
   }
 `;
 
-class GameSummary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const GameSummary = (props) => {
+  const { createdAt, id, name, status, userNationState } = props;
+  const date = new Date(createdAt);
+  const dateString = date.toLocaleDateString('en-GB', dateDisplayFormat);
 
-  render() {
-    const { createdAt, id, name, status, userNationState } = this.props;
-    const date = new Date(createdAt);
-    const dateString = date.toLocaleDateString('en-GB', dateDisplayFormat);
+  const renderPlayingAs = (nationState) => {
+    if (!nationState) return null;
+    const { nation } = nationState;
+    const { name: nationName } = nation;
     return (
-      <StyledListItem key={id}>
-        <Link to={`/game/${id}`}>
-          <article>
-            <header>
-              <span className="name">{name}</span>
-            </header>
-            <section>
-              <p className="created-at">
-                <span className="label">Created</span>
-                <time className="value" dateTime={createdAt}>
-                  {dateString}
-                </time>
-              </p>
-              <p className="status">
-                <span className="label">Status</span>
-                <span className="value">{status}</span>
-              </p>
-            </section>
-          </article>
-        </Link>
-      </StyledListItem>
+      <p className="playing-as">
+        <span className="label">Playing as</span>
+        <span className="value">{nationName}</span>
+      </p>
     );
-  }
-}
+  };
+
+  return (
+    <StyledListItem key={id}>
+      <Link to={`/game/${id}`}>
+        <article>
+          <header>
+            <span className="name">{name}</span>
+          </header>
+          <section>
+            <p className="created-at">
+              <span className="label">Created</span>
+              <time className="value" dateTime={createdAt}>
+                {dateString}
+              </time>
+            </p>
+            <p className="status">
+              <span className="label">Status</span>
+              <span className="value">{status}</span>
+            </p>
+            {renderPlayingAs(userNationState)}
+          </section>
+        </article>
+      </Link>
+    </StyledListItem>
+  );
+};
 
 export default connect(null, null)(GameSummary);
