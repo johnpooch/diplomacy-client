a508b1a0e400ea495530deb6f57b6a0f78e78b7e
John McDowell <john.mcdowell@gamer-network.net>
2020-11-04 20:51:36 +0000

    Update Sandbox to use sandboxGameInterface

--- src/views/Sandbox.jsx
+++ src/views/Sandbox.jsx
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import Dialogue from '../components/Dialogue';
 import Map from '../components/Map';
 import sandboxGameData from '../data/standard/sandbox.json';
 
-import { initialOrderState, Order } from '../game/order';
+import { initialGameFormState } from '../game/base';
+import SandboxGameInterface from '../game/sandboxGameInterface';
 import { getDenormalizedGameDetail } from '../store/denormalizers';
 import { gameActions, gameSelectors } from '../store/games';
+import { orderActions } from '../store/orders';
+import { pieceActions } from '../store/pieces';
 import { variantActions } from '../store/variants';
 
 const Sandbox = (props) => {
-  const { game, location, prepareSandbox, token } = props;
+  const {
+    addOrder,
+    addPiece,
+    game,
+    location,
+    prepareSandbox,
+    removePiece,
+    token,
+  } = props;
 
-  const [orderForm, setOrderForm] = useState(initialOrderState);
+  const [gameForm, setGameForm] = useState(initialGameFormState);
 
   useEffect(() => {
     prepareSandbox(token);
@@ -22,9 +34,19 @@ const Sandbox = (props) => {
   if (!game) return null;
 
   const turn = game.turns[0];
-  const order = new Order(orderForm, turn, setOrderForm);
+  const gameInterface = new SandboxGameInterface(
+    { addOrder, addPiece, removePiece },
+    gameForm,
+    setGameForm,
+    turn
+  );
 
-  return <Map game={game} order={order} turn={turn} />;
+  return (
+    <div>
+      <Map gameInterface={gameInterface} turn={turn} />
+      <Dialogue gameInterface={gameInterface} />
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
@@ -37,13 +59,37 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
+  const addOrder = (order) => {
+    dispatch(orderActions.addOrder(order));
+  };
+  const addPiece = (territory, type, nation) => {
+    const pieceData = {
+      nation,
+      type,
+      territory,
+      named_coast: null,
+      dislodged: false,
+      dislodged_by: null,
+      attacker_territory: null,
+      must_retreat: false,
+    };
+    dispatch(pieceActions.addPiece(pieceData));
+  };
+  const removePiece = (id) => {
+    dispatch(pieceActions.removePiece(id));
+  };
   const prepareSandbox = (token) => {
     dispatch(variantActions.getVariants({ token })).then(() => {
       dispatch(gameActions.getGameDetail.fulfilled(sandboxGameData));
     });
   };
 
-  return { prepareSandbox };
+  return {
+    addOrder,
+    addPiece,
+    prepareSandbox,
+    removePiece,
+  };
 };
 
 export default connect(
