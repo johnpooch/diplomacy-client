51c910aaed6753f02d7a2c2fc568f496e73890c3
Sam Hayes <sam.hayes.dev@gmail.com>
2021-03-12 00:23:16 +0000

    Fix styled import

--- src/components/OrdersSection.jsx
+++ src/components/OrdersSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
-import styled from '@emotion/styled';
 
 import { Button } from './Button';
 import { getErrors } from '../utils';
